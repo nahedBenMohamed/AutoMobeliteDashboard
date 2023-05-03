a40a02e05e9a1bf04c452fb5693de06c83af6e25
victory-aime <vcivictorydarnelmbenze@gmail.com>
2023-05-03 03:46:30 +0200

    "Mis a jour"

--- src/layouts/sidebar/MenuItems.js
+++ src/layouts/sidebar/MenuItems.js
@@ -1,54 +1,44 @@
 const Menuitems = [
-  {
-    title: "Dashboard",
-    icon: "home",
-    href: "/",
-  },
-  {
-    title: "Buttons",
-    icon: "disc",
-    href: "/buttons",
-  },
-  {
-    title: "Forms",
-    icon: "layout",
-    href: "/forms",
-  },
-  {
-    title: "Alerts",
-    icon: "info",
-    href: "/alerts",
-  },
- {
-    title: "reservations",
-    icon: "cart",
-    href: "/reservation",
-  },
     {
-        title: "Gerer Vehicule",
-        icon: "cars",
-        href: "/gererVehicule",
+        title: "Dashboard",
+        icon: "home",
+        href: "/",
     },
     {
-    title: "Listes Clients",
-    icon: "cars",
-    href: "/ListClients",
-},
-   {
-    title: "BILAN",
-    icon: "",
-    href: "/bilan",
-  },
-  /*{
-    title: "Pagination",
-    icon: "user",
-    href: "/pagination",
-  },*/
-  {
-    title: "Tables",
-    icon: "grid",
-    href: "/table",
-  },
+      title: "Buttons",
+      icon: "disc",
+      href: "/buttons",
+    },
+    {
+      title: "Forms",
+      icon: "layout",
+      href: "/forms",
+    },
+    {
+        title: "Analyse de Donnees",
+        icon: "disc",
+        href: "/Analysedonnees",
+    },
+    {
+        title: "Gestion Clients",
+        icon: "layout",
+        href: "/Gestionclients",
+    },
+    {
+        title: "Gestion Flotte",
+        icon: "info",
+        href: "/Gestionflotte",
+    },
+    {
+        title: "Gestion Reservation",
+        icon: "cart",
+        href: "/Gestionreservation",
+    },
+    {
+      title: "Tables",
+      icon: "grid",
+      href: "/table",
+    },
 ];
 
 export default Menuitems;
