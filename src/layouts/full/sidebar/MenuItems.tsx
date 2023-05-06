948daa76e60244eba23e5649cb13cee297bafe31
victory-aime <vcivictorydarnelmbenze@gmail.com>
2023-05-06 12:38:15 +0200

    "Maj"

---
+++ src/layouts/full/sidebar/MenuItems.tsx
@@ -0,0 +1,65 @@
+import {
+  IconAperture, IconCopy, IconLayoutDashboard,  IconMoodHappy, IconTypography, IconUser,IconCar
+} from '@tabler/icons-react';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+  {
+    navlabel: true,
+    subheader: 'Home',
+  },
+
+  {
+    id: uniqueId(),
+    title: 'Dashboard',
+    icon: IconLayoutDashboard,
+    href: '/dashboard',
+  },
+  {
+    id: uniqueId(),
+    title: 'Accounts',
+    icon: IconUser,
+    href: '/accounts',
+  },
+  {
+    id: uniqueId(),
+    title: 'Cars',
+    icon: IconCar,
+    href: '/car',
+  },
+  {
+    navlabel: true,
+    subheader: 'Utilities',
+  },
+  {
+    id: uniqueId(),
+    title: 'Typography',
+    icon: IconTypography,
+    href: '/utilities/typography',
+  },
+  {
+    id: uniqueId(),
+    title: 'Shadow',
+    icon: IconCopy,
+    href: '/utilities/shadow',
+  },
+  {
+    navlabel: true,
+    subheader: 'Extra',
+  },
+  {
+    id: uniqueId(),
+    title: 'Icons',
+    icon: IconMoodHappy,
+    href: '/icons',
+  },
+  {
+    id: uniqueId(),
+    title: 'Sample Page',
+    icon: IconAperture,
+    href: '/sample-page',
+  },
+];
+
+export default Menuitems;
